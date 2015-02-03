16bc505d40c31a5db3ec95c98dc5fa5f7754c71e
Justin Fargione <justin@lexicalbits.com>
2015-02-02 19:37:44 -0500

    Better clint-based UI

--- keepass_dbcheck/reporter/console_reporter.py
+++ keepass_dbcheck/reporter/console_reporter.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-import sys
+from clint.textui import progress, colored, puts
 from .base import Reporter
 
 class ConsoleReporter(Reporter):
 
     def reset(self):
-        self.counter = 0
+        if hasattr(self, 'bar'):
+            self.bar.done()
+        self.bar = progress.Bar(expected_size=self.password_count)
 
     def next_entry(self, entry_path, entry_password):
-        print("\nTesting {0}".format(entry_path))
+        #print("Testing {0}".format(entry_path))
+        puts(colored.blue("Testing{}".format(entry_path)))
+        self.reset()
 
     def report(self, entry_path, entry_password, test_password, is_match, pw_counter=None):
         if pw_counter is not None:
-            strmax = len(str(self.password_count))
-            strctr = str(pw_counter + 1).zfill(strmax)
-            sys.stdout.write("\r{0}/{1}".format(strctr, self.password_count))
-            sys.stdout.flush()
+            self.bar.show(pw_counter)
 
         if is_match:
-            print("{0} has a matching password".format(entry_path))
+            #print("\n{0} has a matching password".format(entry_path))
+            puts(colored.red("{} has a matching password!".format(entry_path)))
             self.reset()
